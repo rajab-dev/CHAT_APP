126cfbc1908ab5583d637f89512380fbf36cdb4f
rajab.ali <rajab.ali@axabiztech.com>
2025-04-29 13:34:43 +0500

    cors setting

--- BACKEND/utils/generateToken.js
+++ BACKEND/utils/generateToken.js
@@ -10,7 +10,7 @@ const generateTokenAndSetCookie = (userid, res) => {
 
       maxAge: 15 * 24 * 60 * 60 * 1000,
       httpOnly: true,
-      sameSite: "strict",
+      sameSite: "none",
       secure: process.env.NODE_ENV !== "development"
     })
 
