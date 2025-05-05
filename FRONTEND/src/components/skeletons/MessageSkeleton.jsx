343024547947b43b54374d898c01622665cb30bd
rajab-dev <mughalrajab26@gmail.com>
2025-05-05 18:49:16 +0500

    set skeleton

--- FRONTEND/src/components/skeletons/MessageSkeleton.jsx
+++ FRONTEND/src/components/skeletons/MessageSkeleton.jsx
@@ -24,14 +24,64 @@ const MessageSkeleton = () => {
 			<div className='flex flex-col gap-1'>
 			  <div className='skeleton h-3 md:h-4 w-32 md:w-40'></div>
 			  <div className='skeleton h-3 md:h-4 w-24 md:w-40'></div>
+			  <div className='skeleton h-3 md:h-4 w-32 md:w-40'></div>
 			</div>
 		  </div>
+
 		  
 		  {/* Outgoing message skeleton */}
 		  <div className='flex gap-2 md:gap-3 items-center justify-end mb-2 md:mb-3'>
 			<div className='flex flex-col gap-1 items-end'>
 			  <div className='skeleton h-3 md:h-4 w-32 md:w-40'></div>
+			  <div className='skeleton h-3 md:h-4 w-32 md:w-40'></div>
 			</div>
+			
+			<div className='skeleton w-8 h-8 md:w-10 md:h-10 rounded-full shrink-0'></div>
+		  </div>
+
+
+		  		  {/* Incoming message skeleton */}
+					<div className='flex gap-2 md:gap-3 items-center mb-2 md:mb-3'>
+			<div className='skeleton w-8 h-8 md:w-10 md:h-10 rounded-full shrink-0'></div>
+			<div className='flex flex-col gap-1'>
+			  <div className='skeleton h-3 md:h-4 w-32 md:w-40'></div>
+			  <div className='skeleton h-3 md:h-4 w-24 md:w-40'></div>
+			  {/* <div className='skeleton h-3 md:h-4 w-32 md:w-40'></div> */}
+			</div>
+		  </div>
+
+		  
+		  {/* Outgoing message skeleton */}
+		  <div className='flex gap-2 md:gap-3 items-center justify-end mb-2 md:mb-3'>
+			<div className='flex flex-col gap-1 items-end'>
+			  <div className='skeleton h-3 md:h-4 w-32 md:w-40'></div>
+			  <div className='skeleton h-3 md:h-4 w-32 md:w-40'></div>
+			  <div className='skeleton h-3 md:h-4 w-32 md:w-40'></div>
+			</div>
+			
+			<div className='skeleton w-8 h-8 md:w-10 md:h-10 rounded-full shrink-0'></div>
+		  </div>
+
+
+		   {/* Incoming message skeleton */}
+		   <div className='flex gap-2 md:gap-3 items-center mb-2 md:mb-3'>
+			<div className='skeleton w-8 h-8 md:w-10 md:h-10 rounded-full shrink-0'></div>
+			<div className='flex flex-col gap-1'>
+			  <div className='skeleton h-3 md:h-4 w-32 md:w-40'></div>
+			  <div className='skeleton h-3 md:h-4 w-24 md:w-40'></div>
+			  {/* <div className='skeleton h-3 md:h-4 w-32 md:w-40'></div> */}
+			</div>
+		  </div>
+
+		  
+		  {/* Outgoing message skeleton */}
+		  <div className='flex gap-2 md:gap-3 items-center justify-end mb-2 md:mb-3'>
+			<div className='flex flex-col gap-1 items-end'>
+			  <div className='skeleton h-3 md:h-4 w-32 md:w-40'></div>
+			  <div className='skeleton h-3 md:h-4 w-32 md:w-40'></div>
+			  <div className='skeleton h-3 md:h-4 w-32 md:w-40'></div>
+			</div>
+			
 			<div className='skeleton w-8 h-8 md:w-10 md:h-10 rounded-full shrink-0'></div>
 		  </div>
 		</>
--- FRONTEND/vite.config.js
+++ FRONTEND/vite.config.js
@@ -8,8 +8,8 @@ export default defineConfig({
     port:3000,
     proxy:{
       "/api":{
-        target: "https://chat-app-fr04.onrender.com"
-        // target: "http://localhost:5000"
+        // target: "https://chat-app-fr04.onrender.com"
+        target: "http://localhost:5000"
       }
     }
   }
